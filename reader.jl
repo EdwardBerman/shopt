d37bbd21dc7bd71bf3962c4cae4705bd8b19a8e2
EdwardBerman <10emb29@gmail.com>
2023-07-09 21:12:44 -0400

    First draft at a distinct reader for summary.shopt easily used by the end user

---
+++ reader.jl
@@ -0,0 +1,47 @@
+
+function read_shopt(shoptFile)
+  using PyCall
+  py"""
+  f = fits.open(shoptFile)
+  polyMatrix = f[0].data
+  """
+  polynomialMatrix = convert(Array{Float64,3}, py"polyMatrix")
+  #degree = degree ...
+  return polynomialMatrix, degree
+end
+
+function p(u,v, polMatrix, degree)
+  #Read in degree of Polynomial
+  #Read in Size of Star Vignet
+  r,c = size(f,1), size(f,2)
+  star = zeros(r,c)
+    
+  for a in 1:r
+    for b in 1:c
+
+      function objective_function(p, x, y, degree)
+        value = 0
+        counter = 0
+          
+        for i in 1:(degree + 1)
+          for j in 1:(degree + 1)
+            if (i - 1) + (j - 1) <= degree
+              counter += 1
+              value += p[counter] * x^(i - 1) * y^(j - 1) 
+            end
+          end
+        end
+        return value
+      end
+      star[a,b] = objective_function(polMatrix[a,b] ,u,v,degree) # -> Filename Polynomial Matrix
+    end
+  end
+  star = star/sum(star)
+  return star
+end
+
+#=
+Example:
+polyMatrix, deg = read_shopt("summary.shopt")
+p(0.5,0.5,polyMatrix, deg)
+=#
